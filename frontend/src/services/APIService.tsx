e6ab59eba58676b269c7ca3a1af4bb677f6a2c74
JoeSirio <joe.sirio22@gmail.com>
2023-09-16 12:50:26 -0700

    update api url

--- frontend/src/services/APIService.tsx
+++ frontend/src/services/APIService.tsx
@@ -8,7 +8,7 @@ export class APIService {
 
     async GetJoke(user_input: string) {
         try{
-            let response = await fetch('http://35.153.157.201/api/joke/' + encodeURIComponent(user_input))
+            let response = await fetch('http://35.153.157.201:8000/api/joke/' + encodeURIComponent(user_input))
             return await response.json();
         }
         catch(error){
